47b68e16f969e26956ce651a52562bcb4fb9826e
Mehdi Adeli <adelisardo@gmail.com>
2025-03-09 22:39:21 +0330

    Fix an issue in Customer config file

--- src/CleanRobust.Infrastructure/Configuration/CustomerConfig.cs
+++ src/CleanRobust.Infrastructure/Configuration/CustomerConfig.cs
@@ -7,5 +7,29 @@ public class CustomerConfig : IEntityTypeConfiguration<Customer>
 {
     public void Configure(EntityTypeBuilder<Customer> builder)
     {
+        builder.Property(p => p.Firstname)
+            .HasMaxLength(20)
+            .IsRequired();
+
+        builder.Property(p => p.Lastname)
+            .HasMaxLength(20)
+            .IsRequired();
+
+        builder.Property(p => p.DateOfBirth)
+            .IsRequired();
+
+        builder.Property(p => p.PhoneNumber)
+            .HasMaxLength(50)
+            .IsUnicode(false)
+            .IsRequired();
+
+        builder.Property(p => p.Email)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(p => p.BankAccountNumber)
+            .HasMaxLength(50)
+            .IsUnicode(false)
+            .IsRequired();
     }
 }
