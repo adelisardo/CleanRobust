4481c17b2fbb7a80fc3d062dd269f53041cf5c78
Mehdi Adeli <adelisardo@gmail.com>
2025-03-09 22:45:00 +0330

    Implement all endpoints for the Customer entity.

--- src/CleanRobust.API/Controllers/CustomerController.cs
+++ src/CleanRobust.API/Controllers/CustomerController.cs
@@ -1,22 +1,28 @@
-﻿using CleanRobust.Application;
+﻿using CleanRobust.API.Common;
+using CleanRobust.Application.Customers.Commands.CreateCustomer;
+using CleanRobust.Application.Customers.Commands.DeleteCustomer;
+using CleanRobust.Application.Customers.Commands.UpdateCustomer;
+using CleanRobust.Application.Customers.Queries.GetCustomer;
+using CleanRobust.Application.Customers.Queries.SearchCustomer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanRobust.API.Controllers;
 
-[Route("[controller]")]
-[ApiController]
-public class CustomerController : ControllerBase
+public class CustomerController : BaseController
 {
-    private readonly ICustomerService CustomerService;
-
-    public CustomerController(ICustomerService customerService)
-    {
-        CustomerService = customerService;
-    }
-
     [HttpGet]
-    public ActionResult<IList<Domain.Entities.Customer>> Get()
-    {
-        return Ok(CustomerService.GetAll());
-    }
+    public async Task<ActionResult<List<SearchCustomerDTO>>> Search([FromQuery] SearchCustomerQuery query) => await Mediator.Send(query);
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<GetCustomerDTO>> Get(Guid id) => await Mediator.Send(new GetCustomerQuery(id));
+
+    [HttpPost]
+    public async Task<ActionResult<Guid>> Create(CreateCustomerCommand command) => await Mediator.Send(command);
+
+    [HttpPut]
+    public async Task<ActionResult<Guid>> Update(UpdateCustomerCommand command) => await Mediator.Send(command);
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<int>> Delete(Guid id) => await Mediator.Send(new DeleteCustomerCommand(id));
+
 }
