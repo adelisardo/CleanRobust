4b915d5bee564ee1e3b1334733255785c3527ffe
Mehdi Adeli <adelisardo@gmail.com>
2025-03-10 20:13:21 +0330

    Add some Application tests function.

---
+++ src/CleanRobust.Application.UnitTests/Customer/CreateCustomerHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Xunit.Categories;
+
+namespace CleanRobust.Application.UnitTests.Customer;
+
+[UnitTest]
+public class CreateCustomerHandlerTests
+{
+    [Fact]
+    public async Task Add_ValidCommand_ShouldReturnsCreatedResult()
+    {
+
+    }
+
+
+    [Fact]
+    public async Task Add_DuplicateEmailCommand_ShouldReturnsFailResult()
+    {
+
+    }
+
+    [Fact]
+    public async Task Add_InvalidCommand_ShouldReturnsFailResult()
+    {
+    }
+}
