df6931ffe06ec897de4438378e996176420f30f8
Mehdi Adeli <adelisardo@gmail.com>
2025-03-10 12:11:22 +0330

    Add a DbSet for EventStores to the IAppUnitOfWork in Application Layer.

--- src/CleanRobust.Application/Common/IAppUnitOfWork.cs
+++ src/CleanRobust.Application/Common/IAppUnitOfWork.cs
@@ -1,7 +1,10 @@
-﻿
+﻿using CleanRobust.Domain.Common;
+using CleanRobust.Domain.Entities.CustomerAggregate;
+
 namespace CleanRobust.Application.Common;
 
 public interface IAppUnitOfWork : IUnitOfWork
 {
+    public DbSet<EventStore> EventStores { get; }
     public DbSet<Customer> Customers { get; }
 }
